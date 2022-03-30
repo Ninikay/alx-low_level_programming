5ed5959b99af9a6d483f31de4639007a1dfc96cc
Ninikay <ms.nikeoni@gmail.com>
2022-03-30 15:57:15 +0000

    add file 2-strlen_recursion.c prototype

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,11 +1,9 @@
 #ifndef _MAIN_H_
-#define _MAIN_H_
-
+#define _MAIN_H_                                                                  
 #include <stdio.h>
 
 int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-
-
-#endif
+int _strlen_recursion(char *s);
+                                                                                  #endif
