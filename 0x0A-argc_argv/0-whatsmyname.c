43a1478637c251d0000cfcce56f63734f56816dc
Ninikay <ms.nikeoni@gmail.com>
2022-04-01 20:35:14 +0000

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: Input command line arguments
+ * @argv: Input argument array
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
