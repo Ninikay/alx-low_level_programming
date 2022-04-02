1c9ddff0c07dda94be6619ec443fef3bdbeb09ae
Ninikay <ms.nikeoni@gmail.com>
2022-04-02 01:31:50 +0000

    prototype files

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H_
+#define MAIN_H_
 #include <stdio.h>
 int _putchar(char c);
 int _islower(int c);
@@ -21,5 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
