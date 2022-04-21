0415c0c02fa359674d1e79176d21573b61c82a59
Ninikay <ms.nikeoni@gmail.com>
2022-04-21 22:49:15 +0000

    print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+/**
+ * print - function executed before main
+ * Return: void.
+ */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}	
