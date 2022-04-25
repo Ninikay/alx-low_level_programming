bd3f4b41e1c83c3441bf76d16931aa6f361b9dd3
Ninikay <ms.nikeoni@gmail.com>
2022-04-25 20:53:30 +0000

    all elements printed

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LIST_H
 #define LIST_H
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list node structure
  * @str: Input string
@@ -19,5 +23,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void print(void) __attribute__((constructor)) pmain();
-#endif:
+
+#endif
