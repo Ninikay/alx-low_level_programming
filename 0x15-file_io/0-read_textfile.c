7afbf3c68dbd5792c6d120ebb2f22a4e2a81bf1e
Ninikay <ms.nikeoni@gmail.com>
2022-05-02 20:17:39 +0000

    function that reads and prints text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,29 +9,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, rd, wr;
+	int fd;
+	ssize_t rd, wr;
 	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	file = open(filename, O_RDONLY);
-	if (file == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters + 1);
-	if (buf == NULL)
-		return (0);
-	rd = read(file, buf, letters);
-	if (rd == -1)
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
 
-	buf[letters] = '\0';
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
+	rd = read(fd, buf, letters);
 	wr = write(STDOUT_FILENO, buf, rd);
-	if (wr == -1)
-		return (0);
 
-	close(file);
+	close(fd);
+
 	free(buf);
+
 	return (wr);
 }
