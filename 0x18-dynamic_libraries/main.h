f4d3b799c8e8d20b678761fb7978279028870695
Ninikay <ms.nikeoni@gmail.com>
2022-06-13 21:52:12 +0000

    dynamic library

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,6 +24,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 
 #endif /* MAIN_H */
